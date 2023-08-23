2e1c2a9edb146e1c723c77a59d3e4b3f98262628
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-23 22:34:19 +0300

    bazı küçük güncellemeler yapıldı.

--- src/modules/layout/card.js
+++ src/modules/layout/card.js
@@ -1,26 +1,27 @@
 import productsData from '../../products.json';
-import {useEffect, useState} from "react";
+
 
 const CardJS = () => {
     const products = productsData.Root.Urunler.Urun;
-
+    const vintageProducts = products.filter(product => product.UrunAdi.includes("Vintage"));
     return (
         <div className="product-section">
             <div className="container">
                 <div className="row">
-                    {products.map(product => (
+                    {vintageProducts.map(product => (
                         <div key={product.UrunKartiID} className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
                             <a className="product-item" href={product.UrunUrl}>
                                 <img
                                     src={product.Resimler.Resim[0]}
                                     className="img-fluid product-thumbnail"
                                     alt={product.UrunAdi}
+                                    style={{objectFit: 'cover', height: '400px'}}
                                 />
-                                <h3 className="product-title">{product.UrunAdi}</h3>
+                                <h6 className="product-title">{product.UrunAdi}</h6>
                                 <strong className="product-price">{product.Marka}</strong>
-                                <span className="icon-cross">
-                  <img src="images/cross.svg" className="img-fluid" alt="Cross"/>
-                </span>
+                                <span className="icon-cross mb-2">
+                                    <img src="images/cross.svg" style={{maxWidth: '100%', height: 'auto'}} alt="Cross"/>
+                                </span>
                             </a>
                         </div>
                     ))}
