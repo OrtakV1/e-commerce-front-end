1217b81a49c5e3e31edc2d0aa2d2fc2b28221202
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-23 20:58:59 +0300

    e-commerce website

--- src/modules/layout/card.js
+++ src/modules/layout/card.js
@@ -1,64 +1,32 @@
+import productsData from '../../products.json';
+import {useEffect, useState} from "react";
+
 const CardJS = () => {
+    const products = productsData.Root.Urunler.Urun;
+
     return (
         <div className="product-section">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                            <img
-                                src="images/product-1.png"
-                                className="img-fluid product-thumbnail"
-                            />
-                            <h3 className="product-title">Nordic Chair</h3>
-                            <strong className="product-price">$50.00</strong>
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid"/>
-                             </span>
-                        </a>
-                    </div>
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                            <img
-                                src="images/product-2.png"
-                                className="img-fluid product-thumbnail"
-                            />
-                            <h3 className="product-title">Kruzo Aero Chair</h3>
-                            <strong className="product-price">$78.00</strong>
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid"/>
-                            </span>
-                        </a>
-                    </div>
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                            <img
-                                src="images/product-3.png"
-                                className="img-fluid product-thumbnail"
-                            />
-                            <h3 className="product-title">Ergonomic Chair</h3>
-                            <strong className="product-price">$43.00</strong>
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid"/>
-                            </span>
-                        </a>
-                    </div>
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                            <img
-                                src="images/product-1.png"
-                                className="img-fluid product-thumbnail"
-                            />
-                            <h3 className="product-title">Nordic Chair</h3>
-                            <strong className="product-price">$50.00</strong>
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid"/>
-                            </span>
-                        </a>
-                    </div>
+                    {products.map(product => (
+                        <div key={product.UrunKartiID} className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                            <a className="product-item" href={product.UrunUrl}>
+                                <img
+                                    src={product.Resimler.Resim[0]}
+                                    className="img-fluid product-thumbnail"
+                                    alt={product.UrunAdi}
+                                />
+                                <h3 className="product-title">{product.UrunAdi}</h3>
+                                <strong className="product-price">{product.Marka}</strong>
+                                <span className="icon-cross">
+                  <img src="images/cross.svg" className="img-fluid" alt="Cross"/>
+                </span>
+                            </a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-
     )
 }
 
