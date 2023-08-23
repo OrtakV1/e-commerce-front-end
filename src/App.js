a78941b7de29002084092ffb4bab5c1088e602fb
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-23 20:34:51 +0300

    e-commerce-web-site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import NavbarJS from "./modules/layout/navbar";
+import CardJS from "./modules/layout/card";
+import DashboardJS from "./modules/layout/dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarJS/>
+            <DashboardJS/>
+            <CardJS/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/modules/layout/navbar.js
@@ -0,0 +1,80 @@
+const NavbarJS = () => {
+    return (
+        <>
+            <nav
+                className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark"
+                arial-label="Furni navigation bar"
+            >
+                <div className="container">
+                    <a className="navbar-brand" href="index.html">
+                        Furni<span>.</span>
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarsFurni"
+                        aria-controls="navbarsFurni"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarsFurni">
+                        <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="index.html">
+                                    Şekil
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="shop.html">
+                                    Renk
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="about.html">
+                                    Modern Halı Tarzları
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="services.html">
+                                    Premium Özel Desen Kilimler
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="blog.html">
+                                    Mutfak Halıları
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="contact.html">
+                                    Çocuk Halılarrı
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="contact.html">
+                                    İndirimler
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                            <li>
+                                <a className="nav-link" href="#">
+                                    <img src="images/user.svg"/>
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav-link" href="cart.html">
+                                    <img src="images/cart.svg"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavbarJS
