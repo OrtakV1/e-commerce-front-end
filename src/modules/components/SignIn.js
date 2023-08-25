3ee5c55fad5a6a4fd8fa225e31b8cbf0b6b02022
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-25 19:44:14 +0300

    signin güncellendi.

--- src/modules/components/SignIn.js
+++ src/modules/components/SignIn.js
@@ -8,69 +8,65 @@ const SignInJS = () => {
     }
 
     return (
-        <div className="page-header align-items-start min-vh-100 d-flex flex-column justify-content-center"
-             style={{background: "#3b5d50"}}
-        >
-            <span className="mask bg-gradient-dark opacity-6"/>
-            <div className="container my-auto">
-                <div className="row">
-                    <div className="col-lg-4 col-md-8 col-12 mx-auto">
-                        <div className="card z-index-0 fadeIn3 fadeInBottom">
-                            <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                <div className="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                                    <h4 className="font-weight-bolder text-center mt-2 mb-0">
-                                        Giriş Yap
-                                    </h4>
+        <section className="vh-100" style={{backgroundColor: "#3b5d50"}}>
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div className="card shadow-2-strong" style={{borderRadius: "1rem"}}>
+                            <div className="card-body p-5 text-center">
+                                <h3 className="mb-5">Sign in</h3>
+                                <div className="form-outline mb-4">
+                                    <input
+                                        type="email"
+                                        id="typeEmailX-2"
+                                        className="form-control form-control-lg"
+                                    />
+                                    <label className="form-label" htmlFor="typeEmailX-2">
+                                        Email
+                                    </label>
                                 </div>
-                            </div>
-                            <div className="card-body">
-                                <form className="text-start">
-                                    <div className="input-group input-group-outline my-3">
-                                        <label className="form-label">Email</label>
-                                        <input type="email" className="form-control"/>
-                                    </div>
-                                    <div className="input-group input-group-outline mb-3">
-                                        <label className="form-label">Password</label>
-                                        <input type="password" className="form-control"/>
-                                    </div>
-                                    <div className="form-check form-switch d-flex align-items-center mb-3">
-                                        <input
-                                            className="form-check-input"
-                                            type="checkbox"
-                                            id="rememberMe"
-                                        />
-                                        <label
-                                            className="form-check-label mb-0 ms-2"
-                                            htmlFor="rememberMe"
-                                        >
-                                            Remember me
-                                        </label>
-                                    </div>
-                                    <div className="text-center">
-                                        <button
-                                            type="button"
-                                            className="btn bg-gradient-primary w-100 my-4 mb-2"
-                                            onClick={handleSignIn}
-                                        >
-                                            Giriş Yap
-                                        </button>
-                                    </div>
-                                    <p className="mt-4 text-sm text-center">
-                                        Hesabınız yok mu?
-                                        <a
-                                            href="/#"
-                                            className="text-primary text-gradient font-weight-bold"
-                                        >
-                                            Kaydolun
-                                        </a>
-                                    </p>
-                                </form>
+                                <div className="form-outline mb-4">
+                                    <input
+                                        type="password"
+                                        id="typePasswordX-2"
+                                        className="form-control form-control-lg"
+                                    />
+                                    <label className="form-label" htmlFor="typePasswordX-2">
+                                        Password
+                                    </label>
+                                </div>
+                                {/* Checkbox */}
+                                <div className="form-check d-flex justify-content-start mb-4">
+                                    <input
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        id="form1Example3"
+                                    />
+                                    <label className="form-check-label" htmlFor="form1Example3">
+                                        {" "}
+                                        Remember password{" "}
+                                    </label>
+                                </div>
+                                <button className="btn btn-primary btn-lg btn-block" type="submit"
+                                        onClick={handleSignIn}>
+                                    Giriş Yap
+                                </button>
+                                <hr className="my-4"/>
+                                <button
+                                    className="btn btn-lg btn-block btn-primary"
+                                    style={{backgroundColor: "#dd4b39"}}
+                                    type="submit"
+                                >
+                                    Kayıt Ol
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
 
     );
 }
