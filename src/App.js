4f71815904dc6cf654734aa301a1b8ac9698ff87
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-25 19:55:24 +0300

    signup eklendi.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {PrimeReactProvider} from 'primereact/api';
 import DetailsPage from "./modules/components/DetailsPage";
 import CarpetTypesJS from "./modules/components/CarpetTypes";
-import SignInJS from "./modules/components/SignIn";
+import SignInJS from "./modules/users/SignIn";
+import SignUpJS from "./modules/users/SignUp";
 
 
 function App() {
 
-    return (
-        <PrimeReactProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<DashboardJS/>}/>
-                    <Route path="/signin" element={<SignInJS/>}/>
-                    
-                    <Route path="/details" element={<DetailsPage/>}/>
-                    <Route path="/carpettypes" element={<CarpetTypesJS/>}/>
-                </Routes>
-            </Router>
+  return (
+    <PrimeReactProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DashboardJS/>}/>
+          <Route path="/signin" element={<SignInJS/>}/>
+          <Route path="/signup" element={<SignUpJS/>}/>
+          <Route path="/details" element={<DetailsPage/>}/>
+          <Route path="/carpettypes" element={<CarpetTypesJS/>}/>
+        </Routes>
+      </Router>
 
-        </PrimeReactProvider>
-    );
+    </PrimeReactProvider>
+  );
 }
 
 export default App;
