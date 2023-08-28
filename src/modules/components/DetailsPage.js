8567ba81b38051452ba29e98f3211354bebfc2ca
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-28 14:29:37 +0300

    detay bölümü güncellendi.

--- src/modules/components/DetailsPage.js
+++ src/modules/components/DetailsPage.js
@@ -5,8 +5,10 @@ import productsData from "../../products.json";
 
 const DetailsPage = () => {
   const targetId = localStorage.getItem('id');
+  
   const products = productsData.Root.Urunler.Urun;
   const matchingProduct = products.find(product => product.UrunKartiID === targetId);
+  console.log(matchingProduct);
   const [imageSrc, setImageSrc] = useState(matchingProduct.Resimler.Resim[0]); // Set the initial image source
   const [quantity, setQuantity] = useState(1)
   const [price, setPrice] = useState(0);
@@ -58,49 +60,28 @@ const DetailsPage = () => {
                     />
                   </div>
                   <div className="text-center d-flex justify-content-center mt-3">
+                  {matchingProduct.Resimler.Resim.slice(0, 4).map((resim, index) => (
                     <img
+                      key={index}
                       width={65}
                       height={65}
-                      onClick={() => changeImageReact(matchingProduct.Resimler.Resim[0])}
-                      src={matchingProduct.Resimler.Resim[0]}
+                      onClick={() => changeImageReact(resim)}
+                      src={resim}
                       className="img-thumbnail"
-                      alt="Thumbnail 1"
+                      alt={`Thumbnail ${index + 1}`}
+                      style={{ cursor: "pointer" }}
                     />
-                    <img
-                      width={65}
-                      height={65}
-                      onClick={() => changeImageReact(matchingProduct.Resimler.Resim[1])}
-                      src={matchingProduct.Resimler.Resim[1]}
-                      className="img-thumbnail"
-                      alt="Thumbnail 1"
-                    />
-                    <img
-                      width={65}
-                      height={65}
-                      onClick={() => changeImageReact(matchingProduct.Resimler.Resim[2])}
-                      src={matchingProduct.Resimler.Resim[2]}
-                      className="img-thumbnail"
-                      alt="Thumbnail 1"
-                    />
-                    <img
-                      width={65}
-                      height={65}
-                      onClick={() => changeImageReact(matchingProduct.Resimler.Resim[3])}
-                      src={matchingProduct.Resimler.Resim[3]}
-                      className="img-thumbnail"
-                      alt="Thumbnail 1"
-                    />
+                  ))}
+
                     {/* Add more thumbnails here */}
                   </div>
                 </div>
               </div>
               <div className="col-lg-6">
                 <h2 className="section-title">
-                  Makine Dokuması Pamuklu Dokuma ve Kaymaz Taban, Şönil , Yüksek
-                  Kalite, Uzun Ömürlü ,İnce Yıkanabilir ,Dekoratif Modern Oda
-                  Halısı,Kilim
+                  {matchingProduct.UrunAdi}
                 </h2>
-                <p>Stok Kodu: (WOOBTNY054)</p>
+                <p>Stok Kodu: ({matchingProduct.UrunSecenek.Secenek[0].StokKodu})</p>
                 <div className="row my-3">
                   <div className="col-12 col-md-12">
                     <div className="feature">
@@ -130,30 +111,16 @@ const DetailsPage = () => {
                   />
                   <div className="col-12 col-md-12 col-sm-12">
                     <div className="feature">
-                      <button
-                        style={{ color: "black", border: "1px solid black" }}
-                        className="btn btn-white-outline m-1 size-button"
-                        data-price={50}
-                        onClick={()=>setValue(50)}
-                      >
-                        100x25
-                      </button>
-                      <button
-                        style={{ color: "black", border: "1px solid black" }}
-                        className="btn btn-white-outline m-1 size-button"
-                        data-price={75}
-                        onClick={()=>setValue(75)}
-                      >
-                        100x50
-                      </button>
-                      <button
-                        style={{ color: "black", border: "1px solid black" }}
-                        className="btn btn-white-outline m-1 size-button"
-                        data-price={100}
-                        onClick={()=>setValue(100)}
-                      >
-                        100x75
-                      </button>
+                      {matchingProduct.UrunSecenek.Secenek.map((secenek, index) => (
+                        <button
+                          key={index}
+                          style={{ color: "black", border: "1px solid black" }}
+                          className="btn btn-white-outline m-1 size-button"
+                          onClick={() => setValue(secenek.SatisFiyati)}
+                        >
+                          {secenek.EkSecenekOzellik.Ozellik._Deger}
+                        </button>
+                      ))}
                     </div>
                   </div>
                   <div className="col-12 col-md-12">
